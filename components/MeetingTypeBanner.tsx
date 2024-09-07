94ec02f374ae2a4a0476c7993b56d82e25fb43ea
Alberto Ferreira <albertomtferreira@gmail.com>
2024-09-07 22:36:37 +0100

    Stream implemented

--- components/MeetingTypeBanner.tsx
+++ components/MeetingTypeBanner.tsx
@@ -4,11 +4,54 @@ import React, { useState } from 'react'
 import HomeCard from './HomeCard'
 import { useRouter } from 'next/navigation'
 import MeetingModal from './MeetingModal'
+import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk'
+import { useUser } from '@clerk/nextjs'
+import { useToast } from '@/hooks/use-toast'
+
 
 const MeetingTypeBanner = () => {
   const router = useRouter()
   const [meetingState, setMeetingState] = useState<'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined>()
-  const createMeeting = () => { }
+  const [values, setValues] = useState({
+    dateTime: new Date(),
+    description: '',
+    link: ''
+  })
+  const [callDetails, setCallDetails] = useState<Call>()
+  const { toast } = useToast()
+  const { user } = useUser()
+  const client = useStreamVideoClient()
+  const createMeeting = async () => {
+    if (!client || !user) return;
+    try {
+      if (!values.dateTime) {
+        toast({ title: "Please select a date and time!" })
+        return
+      }
+      const callId = crypto.randomUUID();
+      const callType = 'default';
+      const call = client.call(callType, callId);
+      if (!call) throw new Error("Call not created");
+      const startsAt = values.dateTime.toISOString() || new Date(Date.now()).toISOString()
+      const description = values.description || 'Instant meeting'
+      await call.getOrCreate({
+        data: {
+          starts_at: startsAt,
+          custom: {
+            description,
+          }
+        }
+      })
+      setCallDetails(call)
+      if (!values.description) {
+        router.push(`/meeting/${call.id}`)
+      }
+      toast({ title: "Meeting Created" })
+    } catch (error) {
+      console.log(error)
+      toast({ title: "Failed to create meeting" })
+    }
+  }
 
 
   return (
