b74f87eee6a35ee0a414129cbe066897c998c526
Alberto Ferreira <albertomtferreira@gmail.com>
2024-09-07 14:49:14 +0100

    Sidebar and Navbar

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sidebarLinks = [
+  {
+    label: 'Home',
+    route: '/',
+    imageUrl: '/icons/Home.svg',
+  },
+  {
+    label: 'Upcoming',
+    route: '/upcoming',
+    imageUrl: '/icons/upcoming.svg',
+  },
+  {
+    label: 'Previous',
+    route: '/previous',
+    imageUrl: '/icons/previous.svg',
+  },
+  {
+    label: 'Recordings',
+    route: '/recordings',
+    imageUrl: '/icons/Recordings.svg',
+  },
+  {
+    label: 'Personal Room',
+    route: '/personal-room',
+    imageUrl: '/icons/add-personal.svg',
+  }
+]
